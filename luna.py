893cca965f27c518bd524ed421fb140c73f85472
TheHamkerCat <thehamkercat@gmail.com>
2021-05-08 15:29:38 +0530

    Removed blacklist

--- luna.py
+++ luna.py
@@ -12,8 +12,6 @@ luna = Client(
 )
 
 arq = ARQ(ARQ_API)
-
-blacklisted = []
 mode = None
 
 
@@ -33,11 +31,9 @@ async def repo(_, message):
 @luna.on_message(filters.command("help") & ~filters.edited)
 async def start(_, message):
     user_id = message.from_user.id
-    if user_id in blacklisted:
-        return
     await luna.send_chat_action(message.chat.id, "typing")
     await message.reply_text(
-        "**Only For Owners**\n/shutdown - `Shutdown Luna.`\n/blacklist - `Blacklist A User.`\n/whitelist - `Whitelist A User.`"
+        "/repo - Get Repo Link"
     )
 
 
@@ -49,50 +45,13 @@ async def shutdown(_, message):
     exit()
 
 
-@luna.on_message(filters.command("blacklist") & filters.user(owner_id) & ~filters.edited)
-async def blacklist(_, message):
-    global blacklisted
-    if not message.reply_to_message:
-        await luna.send_chat_action(message.chat.id, "typing")
-        await message.reply_text("Reply To A User's Message To Blacklist.")
-        return
-    victim = message.reply_to_message.from_user.id
-    if victim in blacklisted:
-        await luna.send_chat_action(message.chat.id, "typing")
-        await message.reply_text("Already Blacklisted Lol.")
-        return
-    blacklisted.append(victim)
-    await luna.send_chat_action(message.chat.id, "typing")
-    await message.reply_text("Blacklisted.")
-
-
-@luna.on_message(filters.command("whitelist") & filters.user(owner_id) & ~filters.edited)
-async def whitelist(_, message):
-    global blacklisted
-    if not message.reply_to_message:
-        await luna.send_chat_action(message.chat.id, "typing")
-        await message.reply_text("Reply To A User's Message To Whitelist.")
-        return
-    victim = message.reply_to_message.from_user.id
-    if victim not in blacklisted:
-        await luna.send_chat_action(message.chat.id, "typing")
-        await message.reply_text("Already Whitelisted Lol.")
-        return
-    blacklisted.remove(victim)
-    await luna.send_chat_action(message.chat.id, "typing")
-    await message.reply_text("Whitelisted.")
-
-
 @luna.on_message(
     ~filters.private &
-    ~filters.command("blacklist")
     & ~filters.command("shutdown")
     & ~filters.command("help")
     & ~filters.edited
 )
 async def chat(_, message):
-    if message.from_user.id in blacklisted:
-        return
     if message.reply_to_message:
         if not message.reply_to_message.from_user.id == bot_id:
             return
@@ -128,14 +87,11 @@ async def chat(_, message):
 
 @luna.on_message(
     filters.private
-    & ~filters.command("blacklist")
     & ~filters.command("shutdown")
     & ~filters.command("help")
     & ~filters.edited
 )
 async def chatpm(_, message):
-    if message.from_user.id in blacklisted:
-        return
     if not message.text:
         return
     await luna.send_chat_action(message.chat.id, "typing")
