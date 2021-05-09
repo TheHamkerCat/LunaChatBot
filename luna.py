1a1e3d89cccd926bfbcdca014f5c06b69304d973
Roj Serbest <rojserbest@gmail.com>
2021-05-09 21:36:17 +0300

    Format code

--- luna.py
+++ luna.py
@@ -1,14 +1,19 @@
 import asyncio
 import re
-from config import bot_token, owner_id, bot_id, ARQ_API_BASE_URL as ARQ_API
-from pyrogram import Client, filters
+
+from config import ARQ_API_BASE_URL as ARQ_API
+from config import bot_id
+from config import bot_token
+from config import owner_id
+from pyrogram import Client
+from pyrogram import filters
 from Python_ARQ import ARQ
 
 luna = Client(
-    ":memory:",
+    ':memory:',
     bot_token=bot_token,
     api_id=6,
-    api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e",
+    api_hash='eb06d4abfb49dc3eeb1aeb98ae0f581e',
 )
 
 arq = ARQ(ARQ_API)
@@ -21,43 +26,46 @@ async def getresp(query):
     return response
 
 
-@luna.on_message(filters.command("repo") & ~filters.edited)
+@luna.on_message(filters.command('repo') & ~filters.edited)
 async def repo(_, message):
     await message.reply_text(
-        "[GitHub](https://github.com/thehamkercat/LunaChatBot)"
-        + " | [Group](t.me/PatheticProgrammers)", disable_web_page_preview=True)
-
-
-@luna.on_message(filters.command("help") & ~filters.edited)
-async def start(_, message):
-    user_id = message.from_user.id
-    await luna.send_chat_action(message.chat.id, "typing")
-    await message.reply_text(
-        "/repo - Get Repo Link"
+        '[GitHub](https://github.com/thehamkercat/LunaChatBot)'
+        ' | [Group](t.me/PatheticProgrammers)',
+        disable_web_page_preview=True,
     )
 
 
-@luna.on_message(filters.command("shutdown") & filters.user(owner_id) & ~filters.edited)
+@luna.on_message(filters.command('help') & ~filters.edited)
+async def start(_, message):
+    await luna.send_chat_action(message.chat.id, 'typing')
+    await message.reply_text(
+        '/repo - Get Repo Link',
+    )
+
+
+@luna.on_message(
+    filters.command('shutdown') & filters.user(owner_id) & ~filters.edited,
+)
 async def shutdown(_, message):
-    await luna.send_chat_action(message.chat.id, "typing")
-    await message.reply_text("**Shutted Down!**")
-    print("Exited!")
+    await luna.send_chat_action(message.chat.id, 'typing')
+    await message.reply_text('**Shutted Down!**')
+    print('Exited!')
     exit()
 
 
 @luna.on_message(
     ~filters.private
-    & ~filters.command("shutdown")
-    & ~filters.command("help")
-    & ~filters.edited
+    & ~filters.command('shutdown')
+    & ~filters.command('help')
+    & ~filters.edited,
 )
 async def chat(_, message):
     if message.reply_to_message:
         if not message.reply_to_message.from_user.id == bot_id:
             return
-        await luna.send_chat_action(message.chat.id, "typing")
+        await luna.send_chat_action(message.chat.id, 'typing')
         if not message.text:
-            query = "Hello"
+            query = 'Hello'
         else:
             query = message.text
         if len(query) > 50:
@@ -68,33 +76,33 @@ async def chat(_, message):
         except Exception as e:
             res = str(e)
         await message.reply_text(res)
-        await luna.send_chat_action(message.chat.id, "cancel")
+        await luna.send_chat_action(message.chat.id, 'cancel')
     else:
         if message.text:
             query = message.text
             if len(query) > 50:
                 return
-            if re.search("[.|\n]{0,}[l|L][u|U][n|N][a|A][.|\n]{0,}", query):
-                await luna.send_chat_action(message.chat.id, "typing")
+            if re.search('[.|\n]{0,}[l|L][u|U][n|N][a|A][.|\n]{0,}', query):
+                await luna.send_chat_action(message.chat.id, 'typing')
                 try:
                     res = await getresp(query)
                     await asyncio.sleep(1)
                 except Exception as e:
                     res = str(e)
                 await message.reply_text(res)
-                await luna.send_chat_action(message.chat.id, "cancel")
+                await luna.send_chat_action(message.chat.id, 'cancel')
 
 
 @luna.on_message(
     filters.private
-    & ~filters.command("shutdown")
-    & ~filters.command("help")
-    & ~filters.edited
+    & ~filters.command('shutdown')
+    & ~filters.command('help')
+    & ~filters.edited,
 )
 async def chatpm(_, message):
     if not message.text:
         return
-    await luna.send_chat_action(message.chat.id, "typing")
+    await luna.send_chat_action(message.chat.id, 'typing')
     query = message.text
     if len(query) > 50:
         return
@@ -104,7 +112,7 @@ async def chatpm(_, message):
     except Exception as e:
         res = str(e)
     await message.reply_text(res)
-    await luna.send_chat_action(message.chat.id, "cancel")
+    await luna.send_chat_action(message.chat.id, 'cancel')
 
 
 print(
@@ -113,7 +121,7 @@ print(
 | Luna Started! |
 -----------------
 
-"""
+""",
 )
 
 
--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
 owner_id = 1243703097
-bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
+bot_token = '16901971:AAFqdM_SQE1PB2P1xLr67k'
 bot_id = 1663901971
 
 # Leave it as it is
-ARQ_API_BASE_URL = "https://thearq.tech"
+ARQ_API_BASE_URL = 'https://thearq.tech'
