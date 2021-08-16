446eb0f789b9eea97b88005cc8beef046c34ab30
Tejas61900 <ubaletejas9@gmail.com>
2021-08-16 05:44:05 +0000

    Docker Support

--- sample_config.py
+++ sample_config.py
@@ -12,7 +12,6 @@ if not HEROKU:
 
     bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
     ARQ_API_KEY = "Get this from @ARQRobot"
-
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
     LANGUAGE = "en"
